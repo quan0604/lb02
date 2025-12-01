12cfbaaf2b1c89ec3689ac2adfdc9bcee86d25fb
quan0604 <148461395+quan0604@users.noreply.github.com>
2025-12-01 15:43:19 +0700

    Lab02-03

---
+++ frmFilm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Lab02_03
+{
+    partial class frmFilm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.grpSeats = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtThanhTien = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(296, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Màn Ảnh";
+            // 
+            // grpSeats
+            // 
+            this.grpSeats.Location = new System.Drawing.Point(98, 89);
+            this.grpSeats.Name = "grpSeats";
+            this.grpSeats.Size = new System.Drawing.Size(519, 281);
+            this.grpSeats.TabIndex = 1;
+            this.grpSeats.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(98, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Chọn";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 429);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Huỷ bỏ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(476, 429);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 33);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Thoát";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 392);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Thành tiền: ";
+            // 
+            // txtThanhTien
+            // 
+            this.txtThanhTien.Location = new System.Drawing.Point(211, 389);
+            this.txtThanhTien.Name = "txtThanhTien";
+            this.txtThanhTien.ReadOnly = true;
+            this.txtThanhTien.Size = new System.Drawing.Size(239, 20);
+            this.txtThanhTien.TabIndex = 6;
+            // 
+            // frmFilm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 474);
+            this.Controls.Add(this.txtThanhTien);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grpSeats);
+            this.Controls.Add(this.label1);
+            this.Name = "frmFilm";
+            this.Text = "BÁN VÉ ";
+            this.Load += new System.EventHandler(this.frmFilm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grpSeats;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtThanhTien;
+    }
+}
+
---
+++ frmFilm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab02_03
+{
+    public partial class frmFilm : Form
+    {
+        public frmFilm()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFilm_Load(object sender, EventArgs e)
+        {
+            int seatNumber = 1;
+            int x = 20, y = 30;
+
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    Button btn = new Button();
+                    btn.Width = 50;
+                    btn.Height = 40;
+                    btn.Left = x + col * 55;
+                    btn.Top = y + row * 45;
+                    btn.Text = seatNumber.ToString();
+                    btn.BackColor = Color.White;
+                    btn.Click += btnChooseASeat;
+
+                    grpSeats.Controls.Add(btn);
+                    seatNumber++;
+                }
+            }
+        }
+        private void btnChooseASeat(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (btn.BackColor == Color.White)
+                btn.BackColor = Color.Blue;     // chọn ghế
+            else if (btn.BackColor == Color.Blue)
+                btn.BackColor = Color.White;    // bỏ chọn
+            else if (btn.BackColor == Color.Yellow)
+                MessageBox.Show("Ghế đã được bán!");
+        }
+        private int GetSeatPrice(int seatNum)
+        {
+            if (seatNum <= 5) return 30000;
+            if (seatNum <= 10) return 40000;
+            if (seatNum <= 15) return 50000;
+            return 80000;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (Button btn in grpSeats.Controls)
+            {
+                if (btn.BackColor == Color.Blue)
+                    btn.BackColor = Color.White;
+            }
+
+            txtThanhTien.Text = "Thành tiền: 0 đ";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
